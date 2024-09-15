aae69d8e1162804e35357ef6f87fe4c640b14dcc
Contactsushil <contact.sushilpandey@gmail.com>
2024-09-15 14:17:16 +0530

    Backbend base

---
+++ Backend/app/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from app.api.routes import user, item
+
+app = FastAPI()
+
+app.include_router(user.router, prefix="/users", tags=["users"])
+app.include_router(item.router, prefix="/items", tags=["items"])
