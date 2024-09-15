5b0f2458a2f50f1f6e68d0593eff3f472748a3d6
Contactsushil <contact.sushilpandey@gmail.com>
2024-09-15 15:44:27 +0530

    removed aback

--- Backend/app/api/routes/item.py
+++
@@ -1,9 +0,0 @@
-# app/api/routes/item.py
-
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/items/")
-async def get_items():
-    return [{"item_id": "1", "name": "Item One"}, {"item_id": "2", "name": "Item Two"}]
--- Backend/app/core/database.py
+++
@@ -1,5 +0,0 @@
-import motor.motor_asyncio
-from app.core.config import settings
-
-client = motor.motor_asyncio.AsyncIOMotorClient(settings.MONGODB_URI)
-database = client['mydatabase']
